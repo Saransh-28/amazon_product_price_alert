087d3a38f3ce14fc16177a4d7ab2111237d8a3fd
Saransh-28 <83751767+Saransh-28@users.noreply.github.com>
2022-07-18 10:02:20 +0530

    Update amazon_price_alert.py

--- amazon_price_alert.py
+++ amazon_price_alert.py
@@ -1,3 +1,4 @@
+import smtplib
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -18,17 +19,18 @@ def main_function():
 
     for x, url in enumerate(prod_url):
         page = requests.get(url, headers=HEADERS)
-        soup = BeautifulSoup(page.content, features="lxml")
+        soup = BeautifulSoup(page.content, features="html.parser")
 
         title = soup.find(id='productTitle')[0].get_text().strip()
-
+# Find the price of product
         try:
             price = float(soup.find(id='priceblock_ourprice').get_text().replace('.', '').replace('€', '').replace(',', '.').strip())
         except:
             try:
-                price = float(soup.find(id='priceblock_saleprice').get_text().replace('$', '').replace(',', '').strip())
+                price = float(soup.find(id='priceblock_saleprice').get_text().replace('.', '').replace('$', '').replace(',', '').strip())
             except:
                 price = ''
+# Check for availability
         try:
             soup.select('#availability .a-color-state')[0].get_text().strip()
             stock = 'Out of Stock'
@@ -38,15 +40,36 @@ def main_function():
                 stock = 'Out of Stock'
             except:
                 stock = 'Available'
+# Compare with the set price
         try:
             if price < prod_name.price[x]:
                 print('the price of '+ title +'  is below the given price')
+                send_mail(title , price , url)
         except:
             pass
-            
+# Add data to the log_file to keep the track of the price   
         log = {'date':now,'url':url,'price':price,'availability':stock}
         log_file.append(log)
-    # sleep for 6 hrs
-    time.sleep(21600)
 
-main_function()
+def send_mail(title,price,link):
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    
+    server.login('SENDER EMAIL ID' , 'PASSWORD')
+
+    body = f'Price alert !! \nThe price of ----{title}---- is {price} !!!\ncheck the link{link}'
+
+    server.send_message(
+        'SENDER ID (FROM ID)',
+        'RECEIVER ID (TO ID)',
+        body
+    )
+    print('EMAIL SEND SUCCESSFULLY !! ')
+    server.quit()
+    
+while True:
+    main_function()
+    # sleep for 6 hrs
+    time.sleep(60*60*6)
